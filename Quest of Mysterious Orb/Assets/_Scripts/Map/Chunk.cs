67f9df5c7cac1d60c7a4254a087e1ae2d8252a35
Skellif <jab100101@gmail.com>
2019-05-13 17:23:00 +0200

    Unfinished map

---
+++ Quest of Mysterious Orb/Assets/_Scripts/Map/Chunk.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// klasa do wykrywania i zarządzania chunkami
+/// </summary>
+public class Chunk : MonoBehaviour{
+	public Transform[] chestPoints;	  ///< <summary> Obiekty symbolizujące potencjalne punkty ze skrzyniami </summary>
+	public Transform[] spawnerPoints; ///< <summary> Obiekty symbolizujące potencjalne punkty ze spawnerami </summary>
+
+	/**
+	 * @summary rozsiewa po chunku rudy odpowiednich materiałów (lub kupy śmieci)
+	 * @param oreNumbers ilość rud kazdego typu do rozrzucenia	 
+	*/
+	public void SpreadResources(int[] oreNumbers){
+		///TODO: zrobić to
+	}
+    [System.Serializable]
+	public struct Box{ 
+		public Vector3 size;
+		public Vector3 position;
+		public Collider[] Overlap(Vector3 pos, Quaternion rot, int LayerMask){
+			Vector3 dest = position;
+            float a = -rot.eulerAngles.y * Mathf.PI / 180;
+
+            float cos = Mathf.Cos(a);
+			float sin = Mathf.Sin(a);
+			dest = new Vector3(dest.x * cos - dest.z*sin, 0, dest.x*sin+dest.z*cos);
+			return Physics.OverlapBox(dest+pos, size/2, rot, LayerMask);	
+		}
+	}
+	public Box[] chunkColliders;
+	/// <summary>
+	/// sprawdza kolizje
+	/// </summary>
+	/// <param name="layerMask">maska kolizji</param>
+	/// <param name="simPos">pozycja chunka, dla której jest to liczone</param>
+	/// <param name="rot">rotacja czanka dla której jest tom liczone</param>
+	/// <returns>czy wykryto kolizje</returns>
+	public bool CheckOverlaps(int layerMask, Vector3 simPos, Quaternion rot){ 
+		foreach(Box b in chunkColliders){ 
+			if(b.Overlap(simPos, rot, layerMask).Length > 0)
+				return true;			
+		} 	
+		return false;
+	}
+
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.magenta;
+		foreach (Box b in chunkColliders)
+		{
+			Vector3 dest =  b.position;
+			float a =- transform.rotation.eulerAngles.y*Mathf.PI/180;
+
+			float cos = Mathf.Cos(a);
+			float sin = Mathf.Sin(a);
+			dest = new Vector3(dest.x * cos - dest.z * sin,dest.y, dest.x * sin + dest.z * cos);
+			Gizmos.DrawCube(dest + transform.position, Mathf.Abs(sin) > 0.9f? new Vector3(b.size.z, b.size.y, b.size.x) : new Vector3(b.size.x,b.size.y, b.size.z));
+		}
+		
+	}
+
+
+}
+
---
+++ Quest of Mysterious Orb/Assets/_Scripts/Map/ChunkConnector.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Autor Skellif
+
+public class ChunkConnector : MonoBehaviour {
+
+
+    private static GameObject exit;
+
+    private List<Transform> exits;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    [SerializeField]
+    private Transform[] startChunk;
+    [SerializeField]
+    private Transform[] roomChunk;
+    [SerializeField]
+    private Transform[] corridorChunk;
+    [SerializeField]
+    private Transform[] junctionChunk;
+    [SerializeField]
+    private Transform[] deadEndChunk;
+    [SerializeField]
+    private Transform deadEndWall;
+
+    [SerializeField]
+    private int minL;
+    [SerializeField]
+    private int maxL;
+
+    private Transform nextChunk;
+    private Transform currentChunk;
+
+    private int pathLength;
+
+    private int prevChunkType;
+
+    private void Start()
+    {
+       // Random.InitState(190219981);
+        exits = new List<Transform>();
+        MapStart();
+    }
+
+    //Main path
+
+    public void MapStart()
+    {
+        currentChunk = Instantiate(startChunk[Random.Range(0, startChunk.Length)], new Vector3(0, 0, 0), Quaternion.identity);
+        prevChunkType = 0;
+        pathLength = Random.Range(minL, maxL);
+
+        for (int i = 0; i < pathLength; i++)
+        {
+            NextChunk();
+        }
+
+    }
+
+    private void NextChunk()
+    {
+        PickNextChunk();
+        int childCount = currentChunk.childCount;
+        for (int j = 0; j < childCount; j++)
+        {
+            if(currentChunk.GetChild(j).tag == "Exit")
+            {
+                exits.Add(currentChunk.GetChild(j));
+
+            }
+        }
+        ChooseExit();
+        CreateNextChunk();
+    }
+
+
+    private void ChooseExit()
+    {
+        exit = exits[Random.Range(0, exits.Count)].gameObject;
+        exits.Clear();
+    }
+
+    // Filling exits
+
+
+    // Next chunk
+
+
+    // Main path
+    private void PickNextChunk()
+    {
+        switch (prevChunkType)
+        {
+            case 0:
+            case 1:
+                prevChunkType = Random.Range(2, 3+1);
+                break;
+            case 2:
+                prevChunkType = Random.Range(1, 3 + 1);
+                break;
+            case 3:
+                prevChunkType = Random.Range(1, 2 + 1);
+                break;
+
+        }
+
+        switch (prevChunkType)
+        {
+            case 1:
+                nextChunk = roomChunk[Random.Range(0, roomChunk.Length)];
+                break;
+            case 2:
+                nextChunk = corridorChunk[Random.Range(0, corridorChunk.Length)];
+                break;
+            case 3:
+                nextChunk = junctionChunk[Random.Range(0, junctionChunk.Length)];
+                break;
+
+        }
+    }
+
+    // Collision controll
+
+    // Connector
+
+    private void CreateNextChunk()
+    {
+        Transform entrance = nextChunk;//.GetChild(0);
+
+        Vector3 displacement = new Vector3(
+            entrance.localPosition.x,
+            0,
+            entrance.localPosition.z);
+
+        Quaternion rotation = new Quaternion();
+        rotation.eulerAngles = exit.transform.rotation.eulerAngles;// - currentChunk.rotation.eulerAngles;
+        
+
+       
+        if (rotation.eulerAngles.y >= 179)
+        {
+            displacement.z *= -1;
+
+        }
+        if (rotation.eulerAngles.y <= 181 && rotation.eulerAngles.y >= 89)
+        {
+            displacement.x *= -1;
+        }
+        if ((rotation.eulerAngles.y >= 89 && rotation.eulerAngles.y <= 91)|| (rotation.eulerAngles.y >= 269 && rotation.eulerAngles.y <= 271))
+        {
+            float tmp = displacement.z;
+            displacement.z = displacement.x;
+            displacement.x = tmp;
+
+        }
+        Vector3 positionTMP = new Vector3(exit.transform.position.x - displacement.x, 0, exit.transform.position.z - displacement.z);
+        if(nextChunk.GetComponent<Chunk>().CheckOverlaps(layerMask, positionTMP, rotation))
+        {
+            Debug.Log("Collision");
+        }
+
+        currentChunk = Instantiate(nextChunk, positionTMP, rotation);
+    }
+
+}
