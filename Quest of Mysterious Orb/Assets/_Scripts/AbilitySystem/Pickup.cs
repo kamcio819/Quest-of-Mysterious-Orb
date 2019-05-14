27efa03d89857c8f0214159053e7ac6df0f02480
Equerell <50588848+Equerell@users.noreply.github.com>
2019-05-14 22:18:22 +0200

    Abilties final improvement

--- Quest of Mysterious Orb/Assets/_Scripts/AbilitySystem/Pickup.cs
+++ Quest of Mysterious Orb/Assets/_Scripts/AbilitySystem/Pickup.cs
@@ -32,4 +32,21 @@ public class Pickup : MonoBehaviour
             }
         }
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //ITEM CAN BE ADDED TO INVENTORY
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
 }
--- Quest of Mysterious Orb/Assets/_Scripts/Stats/PlayerStats.cs
+++ Quest of Mysterious Orb/Assets/_Scripts/Stats/PlayerStats.cs
@@ -12,32 +12,26 @@ public class PlayerStats : MonoBehaviour
     public GameObject OrbSlot1;
     public GameObject OrbSlot2;
     public GameObject OrbSlot3;
+    public int i;
 
     void Start()
 {
     PlayerOrbs[0].id = AllOrbs[0].id;
     PlayerOrbs[0].name = AllOrbs[0].name;
-    //PlayerOrbs[0].description = AllOrbs[0].description;
     
     PlayerOrbs[1].id = AllOrbs[1].id;
     PlayerOrbs[1].name = AllOrbs[1].name;
-    //PlayerOrbs[1].description = AllOrbs[1].description;
 
     PlayerOrbs[2].id = AllOrbs[2].id;
     PlayerOrbs[2].name = AllOrbs[2].name;
-    //PlayerOrbs[2].description = AllOrbs[2].description;
-}
+
+    inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
 
      private void Update()
-    //TEST ORBS
+
     {
-        /*
-        {
-            if (transform.childCount <= 0)
-            {
-                inventory.isFull[i] = false;
-            }
-        } */
+
 
         if (Input.GetMouseButtonDown(0)) //Primary button
         UsedOrb(PlayerOrbs[0].id);
@@ -54,35 +48,37 @@ public class PlayerStats : MonoBehaviour
         switch (id)
         {
             case 0:
-                print(OrbSlot1.name);
+                
                 foreach (Transform child in OrbSlot1.transform)
                 {
                     var orbType = child.GetComponent<OrbObject>().OrbData.OrbType;
                     Debug.Log(orbType);
                     GameObject.Destroy(child.gameObject);
+                    
                 }
-                //Destroy(gameObject);
+                inventory.isFull[0] = false;
                 break;
             case 1:
-                print(OrbSlot2.name);
+                
                 foreach (Transform child in OrbSlot2.transform)
                 {
                     var orbType = child.GetComponent<OrbObject>().OrbData.OrbType;
                     Debug.Log(orbType);
                     GameObject.Destroy(child.gameObject);
+                    
                 }
+                inventory.isFull[1] = false;
                 break;
             case 2:
-                print(OrbSlot3.name);
+                
                 foreach (Transform child in OrbSlot3.transform)
                 {
                     var orbType = child.GetComponent<OrbObject>().OrbData.OrbType;
                     Debug.Log(orbType);
                     GameObject.Destroy(child.gameObject);
+                    
                 }
-                break;
-            case 3:
-                print("Used orb 4");
+                inventory.isFull[2] = false;
                 break;
             default:
                 print("Orb Error");
