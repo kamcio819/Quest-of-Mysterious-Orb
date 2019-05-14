0d376da560ad85999be4a81e6ffc95a83331f410
MatPietrzyk <37366138+MatPietrzyk@users.noreply.github.com>
2019-05-14 10:12:50 +0200

    Enemy patrol movements, enemy player follow, turret

---
+++ Quest of Mysterious Orb/Assets/_Scripts/EnemyScripts/Turret.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    private Transform targetPosition;
+    private GameObject target;
+    private bool targetLocked = false;
+    private bool shotReady = true;
+    public float shotTimer = 1f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetLocked)
+        {
+            transform.LookAt(target.transform);
+            shotTimer += Time.deltaTime;
+            if (shotTimer >= 1f)
+                Shoot();
+        
+        }
+
+        
+    }
+
+    void Shoot()
+    {
+        //Shooting the player.
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        target = other.gameObject;
+        targetLocked = true;
+    }
+}
