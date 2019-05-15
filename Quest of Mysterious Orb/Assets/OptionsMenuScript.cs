85e55c3d99086b1e57a6a1abd1bd1c4260f03816
MatPietrzyk <37366138+MatPietrzyk@users.noreply.github.com>
2019-05-15 17:18:05 +0200

    szkielet menu glownego skonczony

---
+++ Quest of Mysterious Orb/Assets/OptionsMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class OptionsMenuScript : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public Rect SliderLocation;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetBrightnesss(float brightness)
+    {
+        RenderSettings.ambientLight = new Color(brightness, brightness, brightness, 1.0f);
+    }
+
+
+
+
+
+
+}
+
