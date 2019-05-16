86985aa8a66a17b04af087f3021afdab49e8d432
Equerell <50588848+Equerell@users.noreply.github.com>
2019-05-17 01:17:45 +0200

    Orb hits, Boss Fireball and Main Character Engine effects added

--- Quest of Mysterious Orb/Assets/ParticleSystem/GreenOrb/TrailTest.cs
+++ Quest of Mysterious Orb/Assets/ParticleSystem/GreenOrb/TrailTest.cs
@@ -8,6 +8,6 @@ public class TrailTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime * 2);
     }
 }
